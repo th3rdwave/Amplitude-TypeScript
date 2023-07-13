b63af67645be9528f46e0c3538a5b68cd83174e0
Janic Duplessis <janicduplessis@gmail.com>
2023-07-13 14:13:15 -0400

    gitpkg

---
+++ lib/typescript/cookie-migration/index.d.ts
@@ -0,0 +1,5 @@
+import { ReactNativeOptions, UserSession } from '@amplitude/analytics-types';
+export declare const parseOldCookies: (apiKey: string, options?: ReactNativeOptions) => Promise<UserSession>;
+export declare const parseTime: (num: string) => number | undefined;
+export declare const decode: (value?: string) => string | undefined;
+//# sourceMappingURL=index.d.ts.map
